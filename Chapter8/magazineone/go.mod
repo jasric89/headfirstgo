fe4cd14d3ca241eced447e3405f38ff99b14374e
jasric89 <jasoncontent@gmail.com>
2023-08-13 18:18:30 +0100

    adding magazineone package

---
+++ Chapter8/magazineone/go.mod
@@ -0,0 +1,3 @@
+module github.com/jasric89/headfirstgo/Chapter8/magazineone
+
+go 1.20
---
+++ Chapter8/magazineone/magazine.go
@@ -0,0 +1,7 @@
+package magazineone
+
+type subscriber struct {
+	name   string
+	rate   float64
+	active bool
+}
